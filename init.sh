779ce5b80681cab11e02c35c0273c0677e8bc1e6
Diego Rivera <diego.rivera@armedia.com>
2022-03-03 15:47:28 -0600

    Fixed checking for the domain configuration

--- init.sh
+++ init.sh
@@ -1,27 +1,142 @@
 #!/bin/bash
 
-set -e
+set -e -o pipefail
 
+DEBUG="false"
+case "${DEBUG,,}" in
+	true | t | yes | y | 1 | on | active | enabled ) DEBUG="true" ;;
+esac
+
+${DEBUG} && set -x
+
+CONF_DIR="/config"
 SUPERVISOR_SAMBA_CONF="/etc/supervisord.d/samba-dc.ini"
 SUPERVISOR_OPENVPN_CONF="/etc/supervisord.d/openvpn.ini"
-EXT_CONF="/etc/samba/external/smb.conf"
+EXT_SMB_CONF="${CONF_DIR}/smb.conf"
+EXT_KRB_CONF="${CONF_DIR}/krb5.conf"
 
-configure () {
+#
+# Set and normalize variables
+#
+DOMAIN="${DOMAIN:-SAMDOM.LOCAL}"
+DOMAINPASS="${DOMAINPASS:-youshouldsetapassword}"
+JOIN="${JOIN:-false}"
+JOINSITE="${JOINSITE:-NONE}"
+MULTISITE="${MULTISITE:-false}"
+NOCOMPLEXITY="${NOCOMPLEXITY:-false}"
+INSECURELDAP="${INSECURELDAP:-false}"
+DNSFORWARDER="${DNSFORWARDER:-NONE}"
+HOSTIP="${HOSTIP:-NONE}"
 
-	# Set variables
-	DOMAIN="${DOMAIN:-SAMDOM.LOCAL}"
-	DOMAINPASS="${DOMAINPASS:-youshouldsetapassword}"
-	JOIN="${JOIN:-false}"
-	JOINSITE="${JOINSITE:-NONE}"
-	MULTISITE="${MULTISITE:-false}"
-	NOCOMPLEXITY="${NOCOMPLEXITY:-false}"
-	INSECURELDAP="${INSECURELDAP:-false}"
-	DNSFORWARDER="${DNSFORWARDER:-NONE}"
-	HOSTIP="${HOSTIP:-NONE}"
+LDOMAIN="${DOMAIN,,}"
+UDOMAIN="${DOMAIN^^}"
+URDOMAIN="${UDOMAIN%%.*}"
+
+IFS="." D2=(${DOMAIN})
+D3=()
+for P in "${D2[@]}" ; do
+	D3+=("DC=${P^^}")
+done
+unset D2
+
+DC=""
+for P in "${D3[@]}" ; do
+	[ -n "${DC}" ] && DC="${DC},"
+	DC="${DC}${P}"
+done
+unset D3
+
+#
+# This function will check to see if the instance can be considered
+# "configured"
+#
+is_configured() {
+	local CANDIDATES=()
+
+	#
+	# Common data
+	#
+	CANDIDATES+=("account_policy.tdb")
+	CANDIDATES+=("netsamlogon_cache.tdb")
+	CANDIDATES+=("registry.tdb")
+	CANDIDATES+=("share_info.tdb")
+	CANDIDATES+=("smbprofile.tdb")
+	CANDIDATES+=("winbindd_cache.tdb")
+	CANDIDATES+=("wins.ldb")
+
+	#
+	# Private data
+	#
+	CANDIDATES+=("private/dns_update_cache")
+	CANDIDATES+=("private/dns_update_list")
+	CANDIDATES+=("private/encrypted_secrets.key")
+	CANDIDATES+=("private/hklm.ldb")
+	CANDIDATES+=("private/idmap.ldb")
+	CANDIDATES+=("private/kdc.conf")
+	CANDIDATES+=("private/krb5.conf")
+	CANDIDATES+=("private/netlogon_creds_cli.tdb")
+	CANDIDATES+=("private/privilege.ldb")
+	CANDIDATES+=("private/sam.ldb")
+	CANDIDATES+=("private/schannel_store.tdb")
+	CANDIDATES+=("private/secrets.keytab")
+	CANDIDATES+=("private/secrets.ldb")
+	CANDIDATES+=("private/secrets.tdb")
+	CANDIDATES+=("private/share.ldb")
+	CANDIDATES+=("private/spn_update_list")
+	CANDIDATES+=("private/wins_config.ldb")
+
+	#
+	# SSL Certificates
+	#
+	CANDIDATES+=("private/tls/ca.pem")
+	CANDIDATES+=("private/tls/cert.pem")
+	CANDIDATES+=("private/tls/key.pem")
+
+	#
+	# Domain configurations
+	#
+	CANDIDATES+=("private/sam.ldb.d/metadata.tdb")
+	CANDIDATES+=("private/sam.ldb.d/CN=CONFIGURATION,${DC}.ldb")
+	CANDIDATES+=("private/sam.ldb.d/CN=SCHEMA,CN=CONFIGURATION,${DC}.ldb")
+	CANDIDATES+=("private/sam.ldb.d/DC=DOMAINDNSZONES,${DC}.ldb")
+	CANDIDATES+=("private/sam.ldb.d/DC=FORESTDNSZONES,${DC}.ldb")
+	CANDIDATES+=("private/sam.ldb.d/${DC}.ldb")
 	
-	LDOMAIN="${DOMAIN,,}"
-	UDOMAIN="${DOMAIN^^}"
-	URDOMAIN="${UDOMAIN%%.*}"
+
+	local PFX="/var/lib/samba"
+	for C in "${CANDIDATES[@]}" ; do
+		C="${PFX}/${C}"
+		[ -e "${C}" ] || return 1
+		[ -f "${C}" ] || return 1
+		[ -r "${C}" ] || return 1
+	done
+
+	#
+	# Domain Policies
+	#
+	local POLICY_DIR="${PFX}/sysvol/${DOMAIN,,}/Policies"
+	[ -e "${POLICY_DIR}" ] || return 1
+	[ -d "${POLICY_DIR}" ] || return 1
+	[ -r "${POLICY_DIR}" ] || return 1
+	[ -x "${POLICY_DIR}" ] || return 1
+
+	#
+	# Is this correct? Can a domain exist with no policies?
+	#
+	local POLICIES=$(find "${POLICY_DIR}" -type f -iname GPT.INI | wc -l)
+	[ "${POLICIES}" -lt 1 ] && return 1
+
+	#
+	# We're fully configured, so we don't have to redo it
+	#
+	echo "Domain data is already configured"
+	return 0
+}
+
+
+if ! is_configured ; then
+
+	echo "Configuring the domain"
 
 	# If multi-site, we need to connect to the VPN before joining the domain
 	if [ "${MULTISITE,,}" == "true" ]; then
@@ -37,16 +152,19 @@ configure () {
 	[ "${HOSTIP}" != "NONE" ] && HOSTIP_OPTION="--host-ip=${HOSTIP}"
 
 	# Set up samba
-	mv /etc/krb5.conf /etc/krb5.conf.orig
-	cat <<-EOF > /etc/krb5.conf
-	[libdefaults]
-	dns_lookup_realm = false
-	dns_lookup_kdc = true
-	default_realm = ${UDOMAIN}
-	EOF
+	if [ ! -f "${EXT_KRB_CONF}" ] ; then
+		mv /etc/krb5.conf /etc/krb5.conf.orig
+		cat <<-EOF > /etc/krb5.conf
+		[libdefaults]
+		dns_lookup_realm = false
+		dns_lookup_kdc = true
+		default_realm = ${UDOMAIN}
+		EOF
+		cp /etc/krb5.conf "${EXT_KRB_CONF}"
+	fi
 
 	# If the finished file isn't there, this is brand new, we're not just moving to a new container
-	if [ ! -f "${EXT_CONF}" ]; then
+	if [ ! -f "${EXT_SMB_CONF}" ]; then
 		mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
 		if [ "${JOIN,,}" == "true" ]; then
 			if [ "${JOINSITE}" == "NONE" ]; then
@@ -84,26 +202,38 @@ configure () {
 				" /etc/samba/smb.conf
 		fi
 		# Once we are set up, we'll make a file so that we know to use it if we ever spin this up again
-		cp /etc/samba/smb.conf "${EXT_CONF}"
-	else
-		cp "${EXT_CONF}" /etc/samba/smb.conf
+		cp /etc/samba/smb.conf "${EXT_SMB_CONF}"
 	fi
-        
-	# Set up supervisor
-	cat <<-EOF > "${SUPERVISOR_SAMBA_CONF}"
-	[program:samba]
-	command=/usr/sbin/samba -i
+fi
+
+#
+# Apply the external configurations
+#
+cp "${EXT_SMB_CONF}" /etc/samba/smb.conf
+
+#
+# TODO: do we want to add Kerberos (krb5.conf) here? Samba has one ...
+#
+cp "${EXT_KRB_CONF}" /etc/krb5.conf
+
+#
+# Set up supervisor
+#
+# We don't care if this is an old or new container ... we do it anyway to ensure
+# configuration consistency
+#
+cat <<-EOF > "${SUPERVISOR_SAMBA_CONF}"
+[program:samba]
+command=/usr/sbin/samba -i
+EOF
+
+if [ "${MULTISITE,,}" = "true" ] ; then
+	[ -n "${VPNPID}" ] kill "${VPNPID}"
+	cat <<-EOF > "${SUPERVISOR_OPENVPN_CONF}"
+	[program:openvpn]
+	command=/usr/sbin/openvpn --config /docker.ovpn
 	EOF
-
-	if [ "${MULTISITE,,}" == "true" ]; then
-		[ -n "${VPNPID}" ] kill "${VPNPID}"
-		cat <<-EOF > "${SUPERVISOR_OPENVPN_CONF}"
-		[program:openvpn]
-		command=/usr/sbin/openvpn --config /docker.ovpn
-		EOF
-	fi
-}
-
-# If the supervisor conf isn't there, we're spinning up a new container
-[ -f "${SUPERVISOR_SAMBA_CONF}" ] || configure
+else
+	rm -f "${SUPERVISOR_OPENVPN_CONF}" &>/dev/null
+fi
 exec /usr/bin/supervisord -n
