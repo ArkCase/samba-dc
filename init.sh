94a3da13b194d93ab1ae61da2b6ae8e93478ce6b
Diego Rivera <diego.rivera@armedia.com>
2022-03-03 16:36:15 -0600

    Modified to properly handle reconfiguration

--- init.sh
+++ init.sh
@@ -46,6 +46,14 @@ for P in "${D3[@]}" ; do
 done
 unset D3
 
+TIMESTAMP="$(date +%Y%m%d-%H%M%S)"
+
+backup_file() {
+	local FILE="${1}"
+	[ -f "${FILE}" ] && mv -vf "${FILE}" "${FILE}.bak-${TIMESTAMP}"
+	return 0
+}
+
 #
 # This function will check to see if the instance can be considered
 # "configured"
@@ -136,6 +144,11 @@ is_configured() {
 
 if ! is_configured ; then
 
+	# Should we do this?
+	echo "Cleaning up any vestigial configurations"
+	rm -rf /var/lib/samba/* /var/log/samba/*
+	tar -C / -xzf /samba-directory-templates.tar.gz
+
 	echo "Configuring the domain"
 
 	# If multi-site, we need to connect to the VPN before joining the domain
@@ -152,58 +165,56 @@ if ! is_configured ; then
 	[ "${HOSTIP}" != "NONE" ] && HOSTIP_OPTION="--host-ip=${HOSTIP}"
 
 	# Set up samba
-	if [ ! -f "${EXT_KRB_CONF}" ] ; then
-		mv /etc/krb5.conf /etc/krb5.conf.orig
-		cat <<-EOF > /etc/krb5.conf
-		[libdefaults]
-		dns_lookup_realm = false
-		dns_lookup_kdc = true
-		default_realm = ${UDOMAIN}
-		EOF
-		cp /etc/krb5.conf "${EXT_KRB_CONF}"
-	fi
+	mv /etc/krb5.conf /etc/krb5.conf.orig
+	cat <<-EOF > /etc/krb5.conf
+	[libdefaults]
+	dns_lookup_realm = false
+	dns_lookup_kdc = true
+	default_realm = ${UDOMAIN}
+	EOF
+	backup_file "${EXT_KRB_CONF}"
+	cp /etc/krb5.conf "${EXT_KRB_CONF}"
 
 	# If the finished file isn't there, this is brand new, we're not just moving to a new container
-	if [ ! -f "${EXT_SMB_CONF}" ]; then
-		mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
-		if [ "${JOIN,,}" == "true" ]; then
-			if [ "${JOINSITE}" == "NONE" ]; then
-				samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
-			else
-				samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site="${JOINSITE}"
-			fi
+	mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
+	if [ "${JOIN,,}" == "true" ]; then
+		if [ "${JOINSITE}" == "NONE" ]; then
+			samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
 		else
-			PROVISION_FLAGS=()
-			[ -n "${HOSTIP_OPTION}" ] && PROVISION_FLAGS+=("${HOSTIP_OPTION}")
-			samba-tool domain provision --use-rfc2307 --domain="${URDOMAIN}" --realm="${UDOMAIN}" --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass="${DOMAINPASS}" "${PROVISION_FLAGS[@]}"
-			if [[ ${NOCOMPLEXITY,,} == "true" ]]; then
-				samba-tool domain passwordsettings set --complexity=off
-				samba-tool domain passwordsettings set --history-length=0
-				samba-tool domain passwordsettings set --min-pwd-age=0
-				samba-tool domain passwordsettings set --max-pwd-age=0
-			fi
+			samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site="${JOINSITE}"
 		fi
-		sed -i "/\[global\]/a \
-			\\\tidmap_ldb:use rfc2307 = yes\\n\
-			wins support = yes\\n\
-			template shell = /bin/bash\\n\
-			winbind nss info = rfc2307\\n\
-			idmap config ${URDOMAIN}: range = 10000-20000\\n\
-			idmap config ${URDOMAIN}: backend = ad\
-			" /etc/samba/smb.conf
-		if [ "${DNSFORWARDER}" != "NONE" ]; then
-			sed -i "/\[global\]/a \
-				\\\tdns forwarder = ${DNSFORWARDER}\
-				" /etc/samba/smb.conf
+	else
+		PROVISION_FLAGS=()
+		[ -n "${HOSTIP_OPTION}" ] && PROVISION_FLAGS+=("${HOSTIP_OPTION}")
+		samba-tool domain provision --use-rfc2307 --domain="${URDOMAIN}" --realm="${UDOMAIN}" --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass="${DOMAINPASS}" "${PROVISION_FLAGS[@]}"
+		if [[ ${NOCOMPLEXITY,,} == "true" ]]; then
+			samba-tool domain passwordsettings set --complexity=off
+			samba-tool domain passwordsettings set --history-length=0
+			samba-tool domain passwordsettings set --min-pwd-age=0
+			samba-tool domain passwordsettings set --max-pwd-age=0
 		fi
-		if [ "${INSECURELDAP,,}" == "true" ]; then
-			sed -i "/\[global\]/a \
-				\\\tldap server require strong auth = no\
-				" /etc/samba/smb.conf
-		fi
-		# Once we are set up, we'll make a file so that we know to use it if we ever spin this up again
-		cp /etc/samba/smb.conf "${EXT_SMB_CONF}"
 	fi
+	sed -i "/\[global\]/a \
+		\\\tidmap_ldb:use rfc2307 = yes\\n\
+		wins support = yes\\n\
+		template shell = /bin/bash\\n\
+		winbind nss info = rfc2307\\n\
+		idmap config ${URDOMAIN}: range = 10000-20000\\n\
+		idmap config ${URDOMAIN}: backend = ad\
+		" /etc/samba/smb.conf
+	if [ "${DNSFORWARDER}" != "NONE" ]; then
+		sed -i "/\[global\]/a \
+			\\\tdns forwarder = ${DNSFORWARDER}\
+			" /etc/samba/smb.conf
+	fi
+	if [ "${INSECURELDAP,,}" == "true" ]; then
+		sed -i "/\[global\]/a \
+			\\\tldap server require strong auth = no\
+			" /etc/samba/smb.conf
+	fi
+	# Once we are set up, we'll make a file so that we know to use it if we ever spin this up again
+	backup_file "${EXT_SMB_CONF}"
+	cp /etc/samba/smb.conf "${EXT_SMB_CONF}"
 fi
 
 #
