f8df2bf06a28f33fc711fc5c3562b220755804e9
Diego Rivera <diego.rivera@armedia.com>
2024-08-07 13:54:16 -0600

    Modified to better fetch the required settings
    This supports both the old model, and the new one

--- test-ready.sh
+++ test-ready.sh
@@ -7,6 +7,27 @@ SCRIPT="$(basename "${SCRIPT}")"
 [ -n "${SECRETS_DIR}" ] || SECRETS_DIR="/app/secrets"
 export SECRETS_DIR
 
+read_setting()
+{
+	local SETTING="${1}"
+	local DEFAULT="${2:-}"
+
+	local RESULT="${DEFAULT}"
+
+	if [ -v "${SETTING}" ] ; then
+		# It's an envvar!! use it!
+		RESULT="${!SETTING}"
+	elif [ -d "${SECRETS_DIR}" ] ; then
+		# No envvar? What about a secret file?
+		local FILE="${SECRETS_DIR}/${SETTING}"
+		[ -e "${FILE}" ] && [ -f "${FILE}" ] && [ -r "${FILE}" ] && RESULT="$(<"${FILE}")"
+	fi
+
+	# Return the final value
+	echo -en "${RESULT}"
+	exit 0
+}
+
 DEBUG="false"
 case "${DEBUG,,}" in
 	true | t | yes | y | 1 | on | active | enabled ) DEBUG="true" ;;
@@ -24,8 +45,13 @@ fi
 echo -e "Port 636/tcp seems to be listening and serving out certificates"
 ${DEBUG} && echo -e "${OUT}"
 
+REALM="$(read_setting "DOMAIN_REALM")"
+ROOT_DN="$(read_setting "DOMAIN_ROOT_DN")"
+USERNAME="$(read_setting "DOMAIN_USERNAME" "Administrator")"
+PASSWORD="$(read_setting "DOMAIN_PASSWORD")"
+
 ${DEBUG} && set -x
-OUT="$(ldapsearch -H ldaps://localhost -D "$(<"${SECRETS_DIR}/DOMAIN_REALM")\\Administrator" -y "${SECRETS_DIR}/DOMAIN_PASSWORD" -s one -b "$(<"${SECRETS_DIR}/DOMAIN_ROOT_DN")" "(objectClass=organizationalUnit)" dn 2>&1)"
+OUT="$(ldapsearch -H ldaps://localhost -D "${REALM}\\${USERNAME}" -y <(echo -n "${PASSWORD}") -s one -b "${ROOT_DN}" "(objectClass=organizationalUnit)" dn 2>&1)"
 RC=${?}
 ${DEBUG} && set +x
 if [ ${RC} -ne 0 ] ; then
