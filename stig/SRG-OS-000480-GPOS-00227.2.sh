c007922e833e08efcc333e772b88e270c8bc9d5b
Diego Rivera <diego.rivera@armedia.com>
2024-09-26 08:20:51 -0600

    Add another STIG remediation

---
+++ stig/SRG-OS-000480-GPOS-00227.2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+cat <<-EOF
+STIG Remediation script for:
+
+anssi    R31
+cis      enable_authselect
+disa     CCI-000213
+hipaa    164.308(a)(1)(ii)(B), 164.308(a)(7)(i), 164.308(a)(7)(ii)(A), 164.310(a)(1), 164.310(a)(2)(i),
+         164.310(a)(2)(ii), 164.310(a)(2)(iii), 164.310(b), 164.310(c), 164.310(d)(1), 164.310(d)(2)(iii)
+nist     AC-3
+ospp     FIA_UAU.1, FIA_AFL.1
+pcidss4  8.3.4
+os-srg   SRG-OS-000480-GPOS-00227
+EOF
+
+var_authselect_profile="sssd"
+
+authselect current
+
+if [ ${?} -ne 0 ] ; then
+    authselect select "${var_authselect_profile}"
+
+    if [ ${?} -ne 0 ] ; then
+        if rpm --quiet --verify pam ; then
+            authselect select --force "${var_authselect_profile}"
+        else
+            echo "authselect is not used but files from the 'pam' package have been altered, so the authselect configuration won't be forced." >&2
+        fi
+    fi
+fi
+
+# Part two...
+
+readarray -t systemaccounts < <(awk -F: '($3 < 1000 && $3 != root \
+  && $7 != "\/sbin\/shutdown" && $7 != "\/sbin\/halt" && $7 != "\/bin\/sync") \
+  { print $1 }' /etc/passwd)
+
+for systemaccount in "${systemaccounts[@]}"; do
+	usermod -s /sbin/nologin "$systemaccount"
+done
